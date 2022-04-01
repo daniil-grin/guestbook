cf8f771a37a896b4ca4b8933803b384ae26cd612
Grin <grindevel@gmail.com>
2022-04-01 23:40:48 +0500

    Added akismet spam checker

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -41,6 +42,7 @@ class ConferenceController extends AbstractController
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
+        SpamChecker $spamChecker,
         string $photoDir
     ): Response {
         $comment = new Comment();
@@ -59,6 +61,15 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            }
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
@@ -67,6 +78,8 @@ class ConferenceController extends AbstractController
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
+        dump($paginator->count());
+        dump(min($paginator->count(), $offset + CommentRepository::PAGINATOR_PER_PAGE));
         return new Response(
             $this->twig->render('conference/show.html.twig', [
                 'conference' => $conference,
